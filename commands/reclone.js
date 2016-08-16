a96412b01f566291b591e9e13bf649c8ea8c6677
justinoboyle <justin@justinoboyle.com>
2016-08-16 18:50:18 -0400

    changes

--- commands/reclone.js
+++ commands/reclone.js
@@ -4,13 +4,14 @@ module.exports = {
     aliases: ['reclone'],
     description: 'Shuts down and re-clones!',
     permissionsRequired: true,
-    allowedUsers: [ bot.owner ],
+    allowedUsers: [bot.owner],
     hideFromHelp: true,
     emit: function (user, command, args, msgObj, platform) {
-        if(!global.production)
-            return bot.reply("This can only be done on a production server.");
+        if (!global.production)
+            return bot.reply(msgObj, "This can only be done on a production server.");
         bot.reply(msgObj, "Be back in a minute or two! 👋");
-        process.exit(0);
-        
+        setTimeout(() => {
+            process.exit(0);
+        }, 2000);
     }
 }
