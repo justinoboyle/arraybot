6e83cde7c678ea96f9df20b08dbab677b126ea95
justinoboyle <justin@justinoboyle.com>
2016-08-16 19:41:26 -0400

    make sure message only gets sent once

--- pullChecker.js
+++ pullChecker.js
@@ -1,12 +1,14 @@
 const exec = require('child_process').exec;
+let stop = false;
 if (global.production)
     setInterval(() => {
         if (global.production)
             exec(`sh ` + __dirname + `/checkForPull.sh`,
                 function (error, stdout, stderr) {
-                    if (global.production)
+                    if (global.production && !stop)
                         if (stdout.toLowerCase().trim() == "need to pull") {
                             bot.sendMessage(bot.owner, "Re-cloning and re-deploying. Be back in a minute or two! 👋");
+                            stop = true;
                             setTimeout(() => {
                                 process.exit(0);
                             }, 1000);
@@ -14,11 +16,11 @@ if (global.production)
                         }
 
                 });
-    }, 7000);
+    }, 3000);
 
 if (global.production)
     setInterval(() => {
-        if (global.production)
+        if (global.production && !stop)
             exec(`git remote update`,
                 (error, stdout, stderr) => { });
     }, 2000);
